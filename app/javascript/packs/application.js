cce2bf608f1cf6ed154a69b03933391fada32311
joaquinmir <joaquinmir99@gmail.com>
2022-01-15 19:11:13 -0300

    Fixing bootstrap compatibility v3

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
-import "css/styles"
+import "../css/styles"
 
 Rails.start()
 Turbolinks.start()
